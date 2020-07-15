8934c776f87e16df3af6a4cfe240b54111f79e37
pht-even <s.mcdaniel0506@gmail.com>
2020-07-15 18:40:51 -0400

    Created a sha256 hashing function along with a verification function for passwords

---
+++ hashPassword.py
@@ -0,0 +1,13 @@
+import hashlib
+
+
+def makeHash(password):
+    return hashlib.sha256(str.encode(password)).hexdigest()
+
+
+def checkPass(password, hash):
+    if makeHash(password) == hash:
+        return True
+
+    return False
+
