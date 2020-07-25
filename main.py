596aa29bfbdf5904bd7fa3273195f88e840c649f
pht-even <s.mcdaniel0506@gmail.com>
2020-07-24 21:59:25 -0400

    Added a zoo admin program specifically for admins Implemented the ability to search, remove and update employee information via admin menu Improved user experience Corrected grammatical mistakes within prompt

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from findKey import *
 from jsonSaveLoad import *
 from hashPassword import *
+from pprint import pprint
 
 # This program was developed for CS-499 at SNHU
 # and is based on the ZooAuthenticator from IT-145.
 # Users are greeted and asked if they are a new employee.
 # New employees register via the input prompts.
 # Once employees are registered, their information is stored
-# to a JSON file. Returning employees check their login
-# Credentials against existing values within the JSON file
+# in the dictionary and added to the employees.json file.
+# Registered users employees check their login credentials against
+# existing values within the JSON file
 
 
 # Variables to address states within the program
@@ -16,6 +18,7 @@ RUNNING = True
 SAVED_RESPONSE = ""
 SECTION = "RealName"
 OPEN_PAGE = ""
+ADMIN = False
 
 # User response is used multiple times and always cleared after use
 userResponse = ""
@@ -27,6 +30,7 @@ userPass = ""
 userJob = ""  # New users only
 passOK = False
 userOK = False
+verify = ""
 
 # Generating the dictionary from the saved JSON file
 ZOO_EMPLOYEES = dict(loadEmployees())
@@ -41,7 +45,7 @@ while RUNNING:
     # Branch depending on yes, no, quit, or invalid answer
     # Creating a new employee
     if userResponse == "1" and SECTION != "Complete":
-        print("New User\n")
+        userResponse = ""
 
         # These sections are for obtaining information via user input
         # Responses must be valid and will ask for reentering if not
@@ -52,7 +56,7 @@ while RUNNING:
             userResponse = input("Please enter your name.")
             SAVED_RESPONSE = userResponse
 
-            print("You have entered:", SAVED_RESPONSE, "as your name. is this correct?")
+            print("You have entered:", SAVED_RESPONSE, "as your name. Is this correct?")
             userResponse = input()
 
             if userResponse.upper() == "Y":
@@ -148,7 +152,6 @@ while RUNNING:
 
     # Login as current employee
     elif userResponse == "2":
-        print("Active User")
 
         # get the username
         while RUNNING and not userOK:
@@ -161,7 +164,7 @@ while RUNNING:
             # Look for the entered username
             else:
                 if userResponse in ZOO_EMPLOYEES:
-                    print("Accepted")
+                    print("Accepted\n")
                     # save the response for later use
                     SAVED_RESPONSE = userResponse
                     userResponse = ""
@@ -186,12 +189,13 @@ while RUNNING:
                     # Loading the specific job page of the employee from a file
                     try:
                         if ZOO_EMPLOYEES[SAVED_RESPONSE]["Position"].upper() == "ADMIN":
+                            ADMIN = True
                             OPEN_PAGE = open("helloAdmin.txt", "r")
 
                         elif ZOO_EMPLOYEES[SAVED_RESPONSE]["Position"].upper() == "VETERINARIAN":
                             OPEN_PAGE = open("helloVet.txt", "r")
 
-                        elif ZOO_EMPLOYEES[SAVED_RESPONSE]["Position"].uuper() == "ZOOKEEPER":
+                        elif ZOO_EMPLOYEES[SAVED_RESPONSE]["Position"].upper() == "ZOOKEEPER":
                             OPEN_PAGE = open("helloZookeeper.txt", "r")
 
                         else:
@@ -203,15 +207,94 @@ while RUNNING:
                     RUNNING = False
                     break
                 else:
-                    print("That password is incorrect, please try again or type \"q\"")
+                    print("That password is incorrect. Please try again or type \"Q\" to quit.")
+            userResponse = ""
 
     # Exit from the service when "Q" is pressed
-    elif userResponse.upper() == "Q" or "3":
+    elif userResponse.upper() == "Q":
         print("Thank you fo using the Megalopolis Zoo Authentication System.\nGoodbye.")
         RUNNING = False
+
     else:
         print(userResponse, "is not a valid response.")
 
 # Show the user their page if it exists
 if OPEN_PAGE != "":
     print(OPEN_PAGE.read())
+
+# Initiate the admin program
+# Uses a menu to update, remove and find employees
+while ADMIN:
+
+    # Start menu
+    print("Please select an option from below:\n1- Find an Employee\n"
+          "2- Update an Employee's Position\n3- Remove an Employee\n4- Log out")
+    userResponse = input()
+    SAVED_RESPONSE = userResponse
+
+    # Search the dictionary for employee key and return the entry
+    if userResponse == "1":
+        print("Please enter the employee's username.")
+        userResponse = input()
+        try:
+            pprint(ZOO_EMPLOYEES[userResponse])
+        except:
+            print("The username you entered could not be found\n")
+
+    # Update an employee's position
+    elif SAVED_RESPONSE == "2":
+
+        print("Please enter the employee's username.")
+        userResponse = input()
+        if userResponse in ZOO_EMPLOYEES:
+            SAVED_RESPONSE = userResponse
+
+            print("Please enter a new position for", SAVED_RESPONSE)
+            userResponse = input()
+
+            print("Do you really want to change", SAVED_RESPONSE + "'s job from",
+                ZOO_EMPLOYEES[SAVED_RESPONSE]["Position"], "to", userResponse + "?\n")\
+
+            verify = input()
+
+            # Updating both the dictionary and json file to reflect new position
+            if verify.upper() == "Y":
+                ZOO_EMPLOYEES[SAVED_RESPONSE]["Position"] = userResponse
+                saveEmployees(ZOO_EMPLOYEES)
+                print("Your changes have been saved.", SAVED_RESPONSE, "is now a", userResponse)
+            else:
+                verify = userResponse = SAVED_RESPONSE = ""
+
+        else:
+            print("The username you entered could not be found.\n")
+
+    # Removing an employee from both dictionary and json file
+    elif SAVED_RESPONSE == "3":
+        print("Please enter the employee's username.")
+        userResponse = input()
+
+        # The changes being made are permanent.
+        if userResponse in ZOO_EMPLOYEES:
+            print("Do you really want to remove", userResponse, "from the employees list?\nThis cannot be undone.")
+            verify = input()
+
+            # Remove the entry from the dictionary, then update the json
+            if verify.upper() == "Y":
+                try:
+                    del ZOO_EMPLOYEES[userResponse]
+                    pprint(ZOO_EMPLOYEES)
+                    saveEmployees(ZOO_EMPLOYEES)
+                    print("The changes have been saved.")
+                except:
+                    print("An error occurred. Changes were not saved.")
+        else:
+            print("The username you entered could not be found.\n")
+
+    # Logging out
+    elif userResponse == "4":
+        ADMIN = False
+        print("Logged out")
+
+    else:
+        userResponse = SAVED_RESPONSE = ""
+
